90f010063bcb84be742b9cdb98006b50c51e33c7
Patrick Schadler <patrick.schadler@gmail.com>
2018-05-25 18:24:45 +0000

    put sensor logic in dedicated module. added express for providing rest interface to get sensordata over service call

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-var sensorLib = require('node-dht-sensor');
-// Setup sensor, exit if failed
-var sensorType = 22; // 11 for DHT11, 22 for DHT22 and AM2302
-var sensorPin  = 4;  // The GPIO pin number for sensor signal
-if (!sensorLib.initialize(sensorType, sensorPin)) {
-    console.warn('Failed to initialize sensor');
-    process.exit(1);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const readData = require('./read-data.js');
+const server = app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+
+app.post('/sensordata', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(getSensorData());
+});
+
+
+function getSensorData() {
+    var readout = readData();
+    return {
+        'temp': readout.temperature.toFixed(1),
+        'humidity': readout.humidity.toFixed(1)
+    };
 }
-// Automatically update sensor value every 2 seconds
-setInterval(function() {
-    var readout = sensorLib.read();
-    
-    console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-    console.log('Humidity:   ', readout.humidity.toFixed(1)    + '%');
-    console.log('Time:   ', new Date().toLocaleString());
-}, 2000);
+
+console.log('Temp is: ' + JSON.stringify(getSensorData()))
