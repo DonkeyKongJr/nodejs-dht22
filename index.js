86ed9b28e47996678beabc6b0993f3ca472b6d86
Patrick Schadler <patrick.schadler@gmail.com>
2018-05-25 19:15:45 +0000

    added CORS to express config

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const server = app.listen(3000, () => console.log('Example app listening on port
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.post('/sensordata', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
