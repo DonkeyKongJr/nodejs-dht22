e9e10787e0863552c6e85c94d17230c1e8dffd84
Patrick Schadler <patrick.schadler@gmail.com>
2018-05-27 15:48:26 +0200

    addd timestamp to return value and full sensor read out data

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-var sensorLib = require('node-dht-sensor');
-// Setup sensor, exit if failed
-var sensorType = 22; // 11 for DHT11, 22 for DHT22 and AM2302
-var sensorPin  = 4;  // The GPIO pin number for sensor signal
-if (!sensorLib.initialize(sensorType, sensorPin)) {
-    console.warn('Failed to initialize sensor');
-    process.exit(1);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const readData = require('./read-data.js');
+const server = app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.post('/sensordata', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(getSensorData());
+});
+
+
+function getSensorData() {
+    var readout = readData();
+    return {
+        'temp': readout.temperature,
+        'humidity': readout.humidity,
+        'timestamp': new Date()
+    };
 }
-// Automatically update sensor value every 2 seconds
-setInterval(function() {
-    var readout = sensorLib.read();
-    
-    console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-    console.log('Humidity:   ', readout.humidity.toFixed(1)    + '%');
-    console.log('Time:   ', new Date().toLocaleString());
-}, 2000);
+
+console.log('Temp is: ' + JSON.stringify(getSensorData()))
